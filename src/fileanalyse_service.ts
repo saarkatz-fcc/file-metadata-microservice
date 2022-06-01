a5916e4966b922e1014da605e4fdb8f9585e5c88
Saar Katz <kats.saar@gmail.com>
2022-06-02 00:55:31 +0300

    Add file metadata microservice business logic

--- src/fileanalyse_service.ts
+++ src/fileanalyse_service.ts
@@ -1,8 +1,15 @@
+import fs from 'node:fs';
 import { Request, Response } from 'express';
 
 
 function fileanalyse_view(req:Request, res:Response) {
-    res.json({ res: 'Hello Wolrd!' });
+    if (req.file) {
+        fs.unlink(req.file.path, () => {}); // Delete the file right away
+        res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size });
+    }
+    else {
+        res.status(400).json({ error: 'no file received' })
+    }
 }
 
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 import fileanalyse_view from './fileanalyse_service';
 
 const router = Router();
+const upload = multer({ storage: multer.diskStorage({}) });
 
-
-router.post('/api/fileanalyse', fileanalyse_view);
+router.post('/api/fileanalyse', upload.single('upfile'), fileanalyse_view);
 
 
 export default router;
